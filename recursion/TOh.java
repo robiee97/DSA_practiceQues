c2d6a02e43d9bb1296eb026ce6b21e2cec7a8253
robiee97 <robinkr.8cl@gmail.com>
2019-09-18 11:58:02 +0530

    dsa

--- recursion/TOh.java
+++ recursion/TOh.java
@@ -3,22 +3,23 @@ public class TOh{
         TOH(3,"A","B","C");
         // TOHmodify(3, "A", "B", "C");
     }
-        public static void TOH(int n,String s, String d, String h) {
+
+    public static void TOH(int n,String s, String d, String h) {
         if(n==0) {
             return ;
         }
         TOH(n-1,s,h,d);
         System.out.println("Move "+n+"th block from "+s+" to "+d);
         TOH(n-1,h,d,s);
-/*
-Move 1th block from A to B
-Move 2th block from A to C
-Move 1th block from B to C
-Move 3th block from A to B
-Move 1th block from C to A
-Move 2th block from C to B
-Move 1th block from A to B
-*/
+        /*
+        Move 1th block from A to B
+        Move 2th block from A to C
+        Move 1th block from B to C
+        Move 3th block from A to B
+        Move 1th block from C to A
+        Move 2th block from C to B
+        Move 1th block from A to B
+        */
     }
 
     public static void TOHmodify(int n,String s, String d, String h) {
@@ -30,28 +31,28 @@ Move 1th block from A to B
         System.out.println("IN"+s+d+h+n);
         TOHmodify(n-1,h,d,s);
         System.out.println("POST"+s+d+h+n);
- /*
-PREABC3
-PREACB2
-PREABC1
-INABC1
-POSTABC1
-INACB2
-PREBCA1
-INBCA1
-POSTBCA1
-POSTACB2
-INABC3
-PRECBA2
-PRECAB1
-INCAB1
-POSTCAB1
-INCBA2
-PREABC1
-INABC1
-POSTABC1
-POSTCBA2
-POSTABC3*/
+    /*
+    PREABC3
+    PREACB2
+    PREABC1
+    INABC1
+    POSTABC1
+    INACB2
+    PREBCA1
+    INBCA1
+    POSTBCA1
+    POSTACB2
+    INABC3
+    PRECBA2
+    PRECAB1
+    INCAB1
+    POSTCAB1
+    INCBA2
+    PREABC1
+    INABC1
+    POSTABC1
+    POSTCBA2
+    POSTABC3*/
     }
 
 }
